cdfb0d484708782ab435b08b067b09ee18a059d7
parmvirchahal <parmvir.chahal@gmail.com>
2016-02-18 02:02:14 +0000

    I am ready to submit, with an exception to part 2.

---
+++ lab04/part2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* ECE1120 - Lab 4 Part 2
+ * by Parmvir Chahal
+ */
+
+/* This program reads an integer and prints it in decimal notation. */
+
+void print_int(int x);
+
+int main ()
+{
+    int x;
+    char digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    
+    printf("Please type in the number to be converted.\n");
+    
+    scanf("%i", &x);
+    
+    print_int(x);
+    
+    return 0;
+}
+
+void print_int(int x)
+{
+    
+}
